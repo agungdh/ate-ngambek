102595de285975422a469a1697362b72f0c426fb
AgungDH <agungdh@live.com>
2025-08-17 14:11:44 +0700

    init

---
+++ src/main/java/id/my/agungdh/atengambek/AteNgambekApplication.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.atengambek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteNgambekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AteNgambekApplication.class, args);
+    }
+
+}
