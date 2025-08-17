36c90b9db1579131af2af47b7955e05efe2bb90d
AgungDH <agungdh@live.com>
2025-08-17 15:48:54 +0700

    feat: ubah HelloController jadi @Controller dan tambah template Thymeleaf
    - Ganti anotasi dari @RestController ke @Controller agar bisa render view
    - Update method hello untuk mengembalikan view "hello" alih-alih string
    - Tambah file templates/hello.html sebagai halaman Thymeleaf sederhana

--- src/main/java/id/my/agungdh/atengambek/controller/HelloController.java
+++ src/main/java/id/my/agungdh/atengambek/controller/HelloController.java
@@ -1,14 +1,16 @@
 package id.my.agungdh.atengambek.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
     @GetMapping
-    public String hello() {
-        return "Hello World";
+    public String hello(Model model) {
+        return "hello";
     }
 }
